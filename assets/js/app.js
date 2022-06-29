4be664e7067b1786d8db24dac56efe9cb4deee0e
Andrea Paez <andrea.paez86@gmail.com>
2022-06-28 23:00:18 -0400

    done

---
+++ assets/js/app.js
@@ -0,0 +1,73 @@
+const charactersContainer = document.querySelector(".characters-container");
+
+
+const fetchData = async () => {
+    try {
+      let fetchData = await fetch(`https://rickandmortyapi.com/api/character`);
+      let data = await fetchData.json();
+      
+      data.results.length = 20;
+      data.results.map(character => new Character(character));
+        console.log(Character);     
+            
+     } catch (error) {
+      console.log(error);
+     }     
+};
+fetchData();
+
+
+class Character {
+  constructor({ id, name, image, gender, species, status }) {
+    this.id = id;
+    this.name = name;
+    this.image = image;
+    this.gender = gender;
+    this.species = species;
+    this.status = status;
+
+    const card = document.createElement("div");
+    card.classList.add("characters-card");
+
+    const cardImage = document.createElement("div");
+    cardImage.classList.add("img-container");
+
+    const characterPhoto = document.createElement("img");
+    characterPhoto.src = this.image;
+
+    const characterId = document.createElement("p");
+    characterId.classList.add("character-id");
+    characterId.textContent = `ID: ${this.id}`;
+
+    const cardName = document.createElement("p");
+    cardName.classList.add("name");
+    cardName.textContent =
+      this.name.charAt(0).toUpperCase() + this.name.slice(1);
+
+    const cardGender = document.createElement("p");
+    cardGender.classList.add("gender");
+    cardGender.textContent = this.gender;
+
+    const cardSpecies = document.createElement("h2");
+    cardSpecies.classList.add("species");
+    cardSpecies.textContent = this.species;
+
+    const cardStatus = document.createElement("p");
+    cardGender.classList.add("status");
+    cardGender.textContent = this.status;
+
+
+    charactersContainer.appendChild(card);
+
+    cardImage.appendChild(characterPhoto);
+    card.appendChild(cardImage);
+    card.appendChild(characterId);
+    card.appendChild(cardName);
+    card.appendChild(cardGender);    
+    card.appendChild(cardSpecies);
+    card.appendChild(cardStatus);
+    
+   
+}
+}
+
